475622b2d72226e537b8f4e708a031a930e0a18c
s-fukuzawaa <s-fukuzawaa@51S50023543.it.bsd405.org>
2018-12-31 16:04:59 -0800

    finished implementing rest of the SAP. ckpt 1 done.

--- WordNet/src/SAP.java
+++ WordNet/src/SAP.java
@@ -40,6 +40,37 @@ public class SAP
     	
     }
     
+    private int[] commonancestoriterator(Iterable<Integer> v, Iterable<Integer> w)
+    {
+    	int[] result= new int[2];
+    	int length=-1;
+    	int common=-1;
+    	
+    	BreadthFirstDirectedPaths vpath= new BreadthFirstDirectedPaths(copy,v);
+    	BreadthFirstDirectedPaths wpath= new BreadthFirstDirectedPaths(copy,w);
+    	
+    	for(int i=0; i<copy.V(); i++)
+    	{
+    		if(vpath.hasPathTo(i)&&wpath.hasPathTo(i))
+    		{
+    			if(common<0)
+    			{
+    				common=i;
+    				length=vpath.distTo(common)+wpath.distTo(common);
+    			}
+    			else if(vpath.distTo(i)+wpath.distTo(i)<length)
+    			{
+    				common=i;
+    				length=vpath.distTo(i)+wpath.distTo(i);
+    			}
+    		}
+    	}
+    	result[0]=common;
+    	result[1]=length;
+    	
+    	return result;
+    }
+    	
     //use breadthFirst method with 1 int
     public int length(int v, int w)
     {
@@ -56,12 +87,12 @@ public class SAP
 
     public int length(Iterable<Integer> v, Iterable<Integer> w)
     {
-    	throw new UnsupportedOperationException();
+    	return commonancestoriterator(v,w)[1];
     }
 
     public int ancestor(Iterable<Integer> v, Iterable<Integer> w)
     {
-    	throw new UnsupportedOperationException();
+    	return commonancestoriterator(v,w)[0];
     }
 
     public static void main(String[] args)
