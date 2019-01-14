ec5cf47bccc7eb67644c7eee5b6cc59a01470f42
s-fukuzawaa <s-fukuzawaa@51S50023543.it.bsd405.org>
2019-01-13 21:22:35 -0800

    went back to highest and rethink. used sap to check if there is commonancestor from second to last vertex and root. unit test passed.

--- WordNet/src/WordNet.java
+++ WordNet/src/WordNet.java
@@ -93,16 +93,16 @@ public class WordNet
         this.hypernyms= new SAP(temp);
         
         Topological t= new Topological(temp);
+
         int root=0;
+        int headcount=0;
         ArrayList<Integer> toporder= new ArrayList<Integer>();
-        ArrayList<Integer> allorder= new ArrayList<Integer>();
         Iterator<Integer> path= t.order().iterator();
-        
+
         while(path.hasNext())
         {
         	int ordercheck=path.next();
         	root=ordercheck;
-        	allorder.add(ordercheck);
         	if(temp.adj(ordercheck).iterator().hasNext())
         	{
         		toporder.add(ordercheck);
@@ -110,14 +110,14 @@ public class WordNet
         	
         }
         
-        DirectedDFS toptobottom= new DirectedDFS(temp,toporder.get(0));
-       
-        if(temp.reverse().adj(allorder.size()-2).iterator().hasNext()&&temp.reverse().adj(allorder.size()-1).iterator().hasNext())
+        if(this.hypernyms.ancestor(toporder.get(toporder.size()-1), root)==-1)
         {
         	throw new java.lang.IllegalArgumentException();
         }
+        DirectedDFS toptobottom= new DirectedDFS(temp,toporder.get(0));
+       
         
-        if(temp.adj(root).iterator().hasNext()==true||!toptobottom.marked(root))
+        if(temp.adj(root).iterator().hasNext()==true||!toptobottom.marked(root)||headcount>1)
         {
         	throw new java.lang.IllegalArgumentException();
         }
@@ -196,8 +196,8 @@ public class WordNet
     // for unit testing of this class
     public static void main(String[] args)
     {
-		String synsetsFile = "testInput/synsets100-subgraph.txt";
-		String hypernymsFile = "testInput/hypernyms100-subgraph.txt";
+		String synsetsFile = "testInput/synsets6.txt";
+		String hypernymsFile = "testInput/hypernyms6InvalidTwoRoots.txt";
 
 		WordNet wordnet = new WordNet(synsetsFile, hypernymsFile);
        /* wordnet.testNouns("municipality", "region");
